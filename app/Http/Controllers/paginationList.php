3f5328813a533d6a635e24f78c72886b36e483fb
Md Sabbir Rahman <56318666+Sabbir-Rahman@users.noreply.github.com>
2021-01-15 23:41:43 +0600

    pagination page and controller created

---
+++ app/Http/Controllers/paginationList.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class paginationList extends Controller
+{
+    //
+    function show()
+    {
+        return view('paginationList');
+    }
+}
---
+++ resources/views/paginationList.blade.php
@@ -0,0 +1,1 @@
+<h2>Welcome from pagination list</h2>
